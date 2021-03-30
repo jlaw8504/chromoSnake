aa9a1fce61ecb29c4c399a398949b802400f61b3
jlaw8504 <josh.lawrimore@gmail.com>
2021-03-30 10:56:18 -0500

    First commit with PyCharm IDE and colorGenerator.py class.

--- colorGenerator.py
+++ colorGenerator.py
@@ -10,8 +10,9 @@ class ChromoSim:
             print(err)
         self.filepath = filepath
         self.mass_list, self.spring_list, self.hinge_list = self.parse_header()
-        self.super_mass_indexes = self.get_super_masses()
-        self.condensin_spring_indexes = self.get_condensin_springs()
+        self.super_bool_array = self.get_super_masses()
+        self.condensin_spring_indexes, self.cohesin_masses_array = self.get_smc_springs_masses()
+        self.mass_labels = self.gen_mass_labels
 
     def parse_header(self):
         import re
@@ -34,6 +35,7 @@ class ChromoSim:
         hinge_pattern = re.compile('^hinge ')
         with open(self.filepath) as f:
             for line in f:
+                line = line.strip()
                 meta_match = meta_pattern.match(line)
                 color_match = color_pattern.match(line)
                 struct_match = struct_pattern.match(line)
@@ -75,11 +77,11 @@ class ChromoSim:
         if SUPER_MASS is not None and SUPER_MASS.dtype.name == 'float64':
             super_masses = mass_masses == SUPER_MASS
         else:
-            super_mass_indexes = mass_masses == mass_masses.max()
+            super_bool_array= mass_masses == mass_masses.max()
 
-        return super_mass_indexes
+        return super_bool_array
 
-    def get_condensin_springs(self):
+    def get_smc_springs_masses(self):
         """
         Identify condensin springs
         params: None
@@ -91,5 +93,108 @@ class ChromoSim:
         start_idxs = spring_array[:, 1].astype(np.int)
         end_idxs = spring_array[:, 2].astype(np.int)
         diff_idxs = end_idxs - start_idxs
-        condensin_spring_indexes = diff_idxs != 1
-        return condensin_spring_indexes
+        condensin_spring_indexes = np.logical_and(diff_idxs != 1, diff_idxs != -15)
+        # cohesin springs will have -15 in diff_idxs
+        mass_array = np.array(self.mass_list)
+        cohesin_spring_indexes = diff_idxs == -15
+        cohesin_springs_start_end_idx = spring_array[cohesin_spring_indexes, 1:3].astype(np.int)
+        cohesin_masses_array = np.zeros((16, mass_array.shape[1], cohesin_springs_start_end_idx.shape[0]),
+                                        dtype=mass_array.dtype)
+        for i, (end, start) in enumerate(cohesin_springs_start_end_idx):
+            cohesin_ring = mass_array[start:end+1, :]
+            cohesin_masses_array[:, :, i] = cohesin_ring
+
+        return condensin_spring_indexes, cohesin_masses_array
+
+    def gen_mass_labels(self):
+        """
+        Create a list of labels for each mass in a simulation. Masses can be "super", "dna", "cohesin", "condensin", or
+        "middle."
+        params: self
+        returns: A list of labels for each mass in a simulation
+        """
+        import numpy as np
+
+        # super massive masses
+        super_mass_indexes = np.array(np.where(self.super_bool_array))
+        super_mass_indexes = super_mass_indexes.reshape([-1, 2])
+        strand_length_array = super_mass_indexes[:, 1] - super_mass_indexes[:, 0] + 1
+        if len(np.unique(strand_length_array)) == 1:
+            strand_length = np.unique(strand_length_array)
+        else:
+            raise Exception("Calculated pericentromere strand lengths are not consistent.")
+
+        if strand_length % 2 == 0:
+            mid_mass_indexes = super_mass_indexes[:, 0] + strand_length/2
+        else:
+            mid_mass_indexes = super_mass_indexes[:, 0] + np.floor(strand_length/2)
+
+        # pair strands for C-loop indexing
+        top_list = []
+        bottom_list = []
+        for pair in super_mass_indexes:
+            mid_bool = np.logical_and(mid_mass_indexes > pair[0], mid_mass_indexes < pair[1])
+            top_half_indexes = [top for top in range(pair[0], mid_mass_indexes[mid_bool].astype(np.int)[0])]
+            bottom_half_indexes = [bottom for bottom in range(mid_mass_indexes[mid_bool].astype(np.int)[0], pair[1]+1)]
+            top_list.append(top_half_indexes)
+            bottom_list.append(bottom_half_indexes)
+        top_array = np.array(top_list)
+        bottom_array = np.array(bottom_list)
+
+        # ensure that the first column of super_mass_indexes all have same z-value
+        mass_array = np.array(self.mass_list)
+        if len(np.unique(mass_array[super_mass_indexes[:, 0], 5])) != 1:
+            raise Exception("Super massive masses are not indexed as expected.")
+
+        # pair strands of super massive beads to correctly label C-loops
+        super_mass_ends = mass_array[super_mass_indexes[:, 0], 3:5].astype(np.float)
+        c_loop_pair_list = []
+        for i, index in enumerate(super_mass_indexes[:, 0]):
+            ref_xy = mass_array[index, 3:5].astype(np.float)
+            dist_array = np.sum((super_mass_ends - ref_xy)**2, axis=1)
+            dist_array[0 == dist_array] = np.nan
+            c_loop_pair_list.append([i, np.nanargmin(dist_array)])
+
+        c_loop_indexes = []
+        for index in range(16):
+            top_c_loop = np.concatenate((top_array[c_loop_pair_list[index][0], :],
+                                         top_array[c_loop_pair_list[index][1]]))
+            bottom_c_loop = np.concatenate((bottom_array[c_loop_pair_list[index][0], :],
+                                            bottom_array[c_loop_pair_list[index][1]]))
+            c_loop_indexes.append(top_c_loop.tolist())
+            c_loop_indexes.append(bottom_c_loop.tolist())
+
+        # cohesin masses
+        cohesin_mass_indexes = self.cohesin_masses_array[:, 1, :].flatten().astype(np.int)
+        cohesin_bool_list = []
+        for mass in self.mass_list:
+            if int(mass[1]) in cohesin_mass_indexes:
+                cohesin_bool_list.append(True)
+            else:
+                cohesin_bool_list.append(False)
+
+        cohesin_bool_array = np.array(cohesin_bool_list)
+        # condensin-bound masses
+        spring_array = np.array(self.spring_list)
+        condensin_spring_array = spring_array[np.where(self.condensin_spring_indexes)]
+        condensin_mass_indexes = condensin_spring_array[:, 1:3].astype(np.int)
+        # create strand, cohesin, condensin-bound, and super mass labels
+
+        mass_labels = []
+        for i, _ in enumerate(self.mass_list):
+            if i in super_mass_indexes:
+                mass_labels.append('super')
+            elif i in cohesin_mass_indexes:
+                mass_labels.append('cohesin')
+            else:
+                for loop_index, index_list in enumerate(c_loop_indexes):
+                    if i in index_list:
+                        if loop_index % 2 == 0:
+                            position = 'top'
+                            chromosome = loop_index/2 + 1
+                        else:
+                            position = 'bottom'
+                            chromosome = (loop_index-1)/2 + 1
+                mass_labels.append('chr' + str(int(chromosome)) + '_' + position)
+
+        return mass_labels
--- main.py
+++ main.py
@@ -9,13 +9,18 @@ mySim = cG.ChromoSim('/home/test/Documents/chromoShake/bin/Video/WTspindle_0_5_f
 mass_array = np.array(mySim.mass_list)
 spring_array = np.array(mySim.spring_list)
 hinge_array = np.array(mySim.hinge_list)
-super_mass_indexes = mySim.super_mass_indexes
+super_mass_indexes = mySim.super_bool_array
 condensin_spring_indexes = mySim.condensin_spring_indexes
 condensin_array = spring_array[condensin_spring_indexes, :]
-print(condensin_array.shape)
+mba = np.logical_or(np.array(mySim.mass_labels) == 'chr1_top', np.array(mySim.mass_labels) == 'super')
+mba = np.logical_or(mba, np.array(mySim.mass_labels) == 'chr2_top')
+print(sum(mba))
 
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.scatter(mass_array[:, 3].astype(np.double)*1e9, mass_array[:, 4].astype(np.double)*1e9, mass_array[:, 5].astype(np.double)*1e9)
-ax.scatter(mass_array[0, 3].astype(np.double)*1e9, mass_array[0, 4].astype(np.double)*1e9, mass_array[0, 5].astype(np.double)*1e9)
+ax.scatter(mass_array[mba, 3].astype(np.double)*1e9, mass_array[mba, 4].astype(np.double)*1e9, mass_array[mba, 5].astype(np.double)*1e9)
+# ax.scatter(mySim.cohesin_masses_array[:,3,:].flatten().astype(np.double)*1e9,
+#     mySim.cohesin_masses_array[:,4,:].flatten().astype(np.double)*1e9,
+#     mySim.cohesin_masses_array[:,5,:].flatten().astype(np.double)*1e9
+#     )
 plt.show()
