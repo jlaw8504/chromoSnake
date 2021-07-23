543f89cf04ab0a28fd9d0d000ce041d4b8d91672
jlaw8504 <josh.lawrimore@gmail.com>
2021-07-23 17:10:06 -0500

    Altered tensionSim.py class TensionSim to utilize ChromoSim class from colorGenerator and to print out tension CSV file rather than store information in memory

--- tensionSim.py
+++ tensionSim.py
@@ -1,8 +1,9 @@
 import re
 from math import sqrt
-import numpy as np
+from colorGenerator import ChromoSim
 
-class TensionSim():
+
+class TensionSim:
     """
     A class for exploring tension in chromoShake simulations
     """
@@ -14,12 +15,12 @@ class TensionSim():
         except IOError as err:
             print(err)
         self.filepath = filepath
-        self.sim_dict = self.parse_sim()
 
-    def parse_sim(self):
+    def parse_sim(self, outfile):
         """
         Parse an entire chromoShake outfile in one pass
         """
+
         def calc_paired_distance(time_dict, mass_idx1, mass_idx2):
             """
             Parse the time_dict with mass_idx1 and mass_idxs vars to calculate distance between 2 masses
@@ -46,83 +47,69 @@ class TensionSim():
         color_pattern = re.compile('^MassColors')
         struct_pattern = re.compile('^structure')
         timeline_pattern = re.compile('^Time ')
-        mass_pattern = re.compile('^mass ')
-        spring_pattern = re.compile('^spring ')
-        hinge_pattern = re.compile('^hinge ')
-        # instantiate nested dictionary
-        sim_dict = dict()
-        sim_dict['struct'] = dict()
+        # use ChromoSim class to parse header
+        my_sim_header = ChromoSim(self.filepath)
         # begin parsing the file
         with open(self.filepath) as f:
-            for line in f:
-                line = line.strip()
-                meta_match = meta_pattern.match(line)
-                color_match = color_pattern.match(line)
-                struct_match = struct_pattern.match(line)
-                timeline_match = timeline_pattern.match(line)
-                if in_color and timeline_match is not None:
-                    in_timeline = True
-                    num_masses = len(sim_dict['struct'])
-                    in_color = False
-                if in_struct and color_match is not None:
-                    in_color = True
-                    in_struct = False
-                if in_meta and struct_match is not None:
-                    in_struct = True
-                    in_meta = False
-                if in_meta and meta_match is None:
-                    raise Exception('File does not start with metadata section. Is this a chromoShake outfile?')
-                if in_struct:
-                    if mass_pattern.match(line) is not None:
-                        mass_list = line.split()
-                        mass_idx = mass_list[1]
-                        sim_dict['struct'][mass_idx] = {
-                            'mass_kg': float(mass_list[2]),
-                            'spring_idx_list': [],
-                            'hinge_idx_list': []
+            with open(outfile, 'w') as f_out:
+                for line in f:
+                    line = line.strip()
+                    meta_match = meta_pattern.match(line)
+                    color_match = color_pattern.match(line)
+                    struct_match = struct_pattern.match(line)
+                    timeline_match = timeline_pattern.match(line)
+                    if in_color and timeline_match is not None:
+                        in_timeline = True
+                        num_masses = len(my_sim_header.mass_list)
+                        in_color = False
+                    if in_struct and color_match is not None:
+                        in_color = True
+                        in_struct = False
+                    if in_meta and struct_match is not None:
+                        in_struct = True
+                        in_meta = False
+                    if in_meta and meta_match is None:
+                        raise Exception('File does not start with metadata section. Is this a chromoShake outfile?')
+                    if in_struct:
+                        # this is now taken care of by the my_sim_header object
+                        continue
+                    if timeline_match and in_timeline:
+                        # store the time
+                        time = line.strip().split()[-1]
+                        # reset the mass counter
+                        mass_cnt = 0
+                        # reset the dictionary
+                        time_dict = dict()
+
+                    if not timeline_match and in_timeline and line:
+                        mass_idx = str(num_masses - 1 - mass_cnt)  # must flip cnt to get index to match header
+                        x_string, y_string, z_string = line.strip().split()
+                        time_dict[mass_idx] = {
+                            'x': x_string,
+                            'y': y_string,
+                            'z': z_string,
                         }
-
-                    elif spring_pattern.match(line) is not None:
-                        spring_list = line.split()
-                        sim_dict['struct'][spring_list[1]]['spring_idx_list'].append(
-                            [spring_list[1], spring_list[2], spring_list[3], spring_list[4]]
-                        )
-                        sim_dict['struct'][spring_list[2]]['spring_idx_list'].append(
-                            [spring_list[1], spring_list[2], spring_list[3], spring_list[4]]
-                        )
-
-                    elif hinge_pattern.match(line) is not None:
-                        hinge_list = line.split()
-                        sim_dict['struct'][hinge_list[2]]['hinge_idx_list'].append(
-                            [hinge_list[1], hinge_list[2], hinge_list[3]]
-                        )
-
-                if timeline_match and in_timeline:
-                    # store the time
-                    time = line.strip().split()[-1]
-                    mass_cnt = 0
-                    parsed_mass_list = []
-                    sim_dict[time] = dict()
-
-                if not timeline_match and in_timeline and line:
-                    mass_idx = str(num_masses - 1 - mass_cnt)  # must flip cnt to get index to match header
-                    x_string, y_string, z_string = line.strip().split()
-                    sim_dict[time][mass_idx] = {
-                        'x': x_string,
-                        'y': y_string,
-                        'z': z_string,
-                    }
-                    mass_cnt += 1
-                    # once all masses are parsed, calculate tension
-                    if mass_cnt == num_masses:
-                        for key in sim_dict['struct']:
-                            tension_list = []
-                            for spring_list_info in sim_dict['struct'][key]['spring_idx_list']:
-                                dist = calc_paired_distance(sim_dict[time], spring_list_info[0], spring_list_info[1])
-                                rest_length = float(spring_list_info[2])
+                        mass_cnt += 1
+                        # once all masses are parsed, calculate tension
+                        if mass_cnt == num_masses:
+                            print(f'time: {time}')
+                            for spring_list_info in my_sim_header.spring_list:
+                                spring_mass_idx_1 = spring_list_info[1]
+                                spring_mass_idx_2 = spring_list_info[2]
+                                rest_length = float(spring_list_info[3])
+                                spring_k = float(spring_list_info[4])
+                                dist = calc_paired_distance(time_dict, spring_mass_idx_1, spring_mass_idx_2)
                                 delta_dist = dist - rest_length
-                                tension_list.append(delta_dist * float(spring_list_info[3]))
-                            tension_array = np.array(tension_list)
-                            sim_dict[time][key]['tension'] = tension_array.mean()
-
-            return sim_dict
+                                tension = delta_dist * spring_k
+                                f_out.write(
+                                    '{0}, {1}, {2}, {3}, {4}, {5}, {6}\n'.format(
+                                        time,
+                                        spring_mass_idx_1,
+                                        spring_mass_idx_2,
+                                        tension,
+                                        my_sim_header.mass_labels[int(spring_mass_idx_1)],
+                                        my_sim_header.mass_labels[int(spring_mass_idx_2)],
+                                        my_sim_header.loop_labels[int(spring_mass_idx_1)],
+                                        my_sim_header.loop_labels[int(spring_mass_idx_2)]
+                                    )
+                                )
