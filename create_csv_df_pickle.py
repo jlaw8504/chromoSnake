c0031fb55b53b559667db6f9d9334bac630ed775
jlaw8504 <josh.lawrimore@gmail.com>
2021-07-30 07:55:06 -0500

    Updated tensionSim.py to include a static method called parse_csv to parse the raw tension CSV-files produced by parse_sim. The parse_csv function will generate a DataFrame with mean tension in pN after the specified equilibraiton time, default 0.02 seconds.

--- create_csv_df_pickle.py
+++ create_csv_df_pickle.py
@@ -16,7 +16,7 @@ names_list = [
     'mass_label_2',
     'is_in_loop_1',
     'is_in_loop_2'
-    ]
+]
 df = pd.read_csv('/home/test/PycharmProjects/chromoSnake/new_test.txt', names=names_list)
 equilibrium_df = df.loc[df['time'] > 0.02]
 del df
@@ -27,8 +27,23 @@ del equilibrium_df
 label_df = label_df.set_index(['spring_idx'])
 label_df.drop(columns=['time', 'tension_N'], inplace=True)
 final_df = label_df.join(mean_calc_df)
+
+
+# add condensin labels
+def id_condensin(df):
+    if abs(df['spring_mass_idx_1'] - df['spring_mass_idx_2']) != 1 and 'cohesin' not in df['mass_label_1']:
+        return True
+    else:
+        return False
+
+
+final_df['is_condensin_spring'] = final_df.apply(id_condensin, axis=1)
+# convert the tension from N to pN
+final_df['tension_pN'] = final_df.apply(lambda x: x['tension_N']*10**12, axis=1)
+final_df.drop(columns=['tension_N'], inplace=True)
+
 with open('/home/test/PycharmProjects/chromoSnake/WTspindle_2_25_coh220_final_df.pkl', 'wb') as f_pkl:
     dump(final_df, f_pkl)
 
 end = dt.datetime.now()
-print((end-start))
+print((end - start))
